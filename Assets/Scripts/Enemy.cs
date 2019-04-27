2b39ed95aa16783dfe8e427774a7754123d4f39f
Huevitoconlimon <mariana.loy.menchaca@gmail.com>
2019-04-26 19:17:40 -0500

    Commit Actividad

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public GameObject enemyMisilePrefab;
+    public float speed = 5.0f;
+    public int timesHited = 0;
+    public int enemyLives = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(7,0,0);
+       //nstantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shoot();
+    }
+
+    private void Movement()
+    {
+        
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (transform.position.x < -10)
+        {
+            float randomY = Random.Range(-7f, 7f);
+            transform.position = new Vector3(30, randomY, 0); 
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(enemyMisilePrefab, transform.position, Quaternion.identity);
+        StartCoroutine(Shooting());
+    }
+
+    IEnumerator Shooting()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Misile")
+        {
+            timesHited++;
+            if(timesHited >= 3)
+            {
+                Destroy(other.gameObject);
+
+                Destroy(this.gameObject);
+            }
+            
+        }
+
+        else if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.Damage();
+                
+            }
+
+           
+        }
+    }
+
+  
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject playerMisilPrefab;
+    public SpriteRenderer ship;
+    public float speed = 5.0f;
+    public int lives = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-7, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(playerMisilPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void Movement()
+    {
+        //transform.Translate(Vector3.right * Time.deltaTime * speed);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.down * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * verticalInput * Time.deltaTime);
+    }
+
+    public void Damage()
+    {
+        lives--;
+        if(lives < 1)
+        {
+            Debug.Log("Game Over");
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Misile")
+        {
+            Damage();
+
+        }
+
+        if (other.tag == "Meteor")
+        {
+            Damage();
+            ChangeToRed();
+
+        }
+
+        if (other.tag == "Enemy")
+        {
+            Damage();
+
+        }
+    }
+    void ChangeToRed()
+    {
+        ship = GetComponent<SpriteRenderer>();
+        ship.color = Color.red;
+        StartCoroutine(ChangeToWhite());
+    }
+
+    IEnumerator ChangeToWhite()
+    {
+        yield return new WaitForSeconds(1);
+        ship.color = Color.white;
+    }
+
+}
